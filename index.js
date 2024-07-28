7ee7a8941eebc721ed49e5517065d609111f1f57
Puso90 <juwishbrands@gmail.com>
2024-07-28 19:42:19 +0200

    final update to price calculations

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function addToOrder(itemName) {
 
     // Calculate and update the total price
         const currentTotal = parseFloat(orderTotal.textContent); 
-        const itemPrice = Math.floor(Math.random() * 100) + 1; 
+        const itemPrice = Math.floor(Math.random() * 1000) + 1; 
         const newTotal = itemPrice + currentTotal; 
         orderTotal.textContent = newTotal.toFixed(2);
 
